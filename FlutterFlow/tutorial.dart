e3e62747b50b3ec2ecb31c046d3df1429e5acd99
abhishek <abhishekmankame962@gmail.com>
2025-08-21 23:01:39 +0530

    dart intro

---
+++ FlutterFlow/tutorial.dart
@@ -0,0 +1,23 @@
+// string
+var name = "Ambitious Abhishek";
+
+// int/number
+var age = 27;
+
+// boolean
+var isMale = true;
+var isMinor = age > 18; // --> Returns true
+
+// Lists
+var fruits = ["apple", "banana", "orange"]; 
+
+var orange = fruits[2]; // -> orange
+
+// Maps/Objects
+
+// key/value pairs
+var resturant = {
+    "name": "McDonalds",
+    "location": "New York",
+    "rating": 4.5
+};
